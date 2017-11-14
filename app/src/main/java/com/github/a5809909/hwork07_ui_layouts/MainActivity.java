725e0a425ff33dacf8faa0d50013a77b8bdf0789
bektop <5809909@mail.ru>
2017-11-14 23:58:45 +0300

    чистый

---
+++ app/src/main/java/com/github/a5809909/hwork07_ui_layouts/MainActivity.java
@@ -0,0 +1,13 @@
+package com.github.a5809909.hwork07_ui_layouts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page);
+    }
+}
